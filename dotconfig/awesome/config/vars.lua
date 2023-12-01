16fa18672d0e60bb54131fe272dfc50158691800
notanyone <98587775+stwawbewwy@users.noreply.github.com>
2023-12-01 17:34:14 +0800

    Add files via upload

--- dotconfig/awesome/config/vars.lua
+++ dotconfig/awesome/config/vars.lua
@@ -1,12 +1,30 @@
 local _M = {}
 
 local awful = require'awful'
+local l = awful.layout.suit
+
+_M.tags = {
+        " Work", 
+        "󰙯 Messaging", 
+        " Terminal", 
+        " Misc", 
+        "󰎄 Music", 
+        "󰊗 Game"
+}
+
+_M.defaultlayouts = {
+        l.fair, 
+        l.fair, 
+        l.fair, 
+        l.floating, 
+        l.floating, 
+        l.floating
+}
 
 _M.layouts = {
         awful.layout.suit.floating,
         awful.layout.suit.fair,
+        awful.layout.suit.max,
 }
-
-_M.tags = {" Work", "󰙯 Messaging", " Terminal", " Misc", "󰎄 Music", "󰊗 Game"}
 
 return _M
